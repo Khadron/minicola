2c6014a28139fad8124cdd9cbe33584ec28381a8
khadron <khadron@163.com>
2019-03-04 11:32:02 +0800

    Improved singleton mode implementation

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,18 +1,7 @@
 const path = require("path");
 const LPC = require('../../lpc');
-
-console.log("进入auth");
-// 单例模式
-const instance = null;
-const getInstance = (function () {
-  if (!instance) {
-    console.log("进入instance：", instance);
-    return LPC(path.join(__dirname, "./tokenHandler"));
-  }
-  return instance;
-})();
-
-module.exports = getInstance;
+console.log("--进入instance--");
+module.exports =  LPC(path.join(__dirname, "./tokenHandler"));
 
 // ES6
 // class AuthServer {
